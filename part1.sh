097fa4ce6cc82669f4028fb23530f4df214bc60b
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-03-25 19:45:09 -0400

    added color mapper

--- part1.sh
+++ part1.sh
@@ -27,6 +27,11 @@
 -file reducer.py -reducer reducer.py \
 -input /p1/input/* -output /p1/output/Q3/
 
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar  \
+-file color_mapper.py -mapper color_mapper.py \
+-file reducer.py -reducer reducer.py \
+-input /p1/input/* -output /p1/output/Q4/
+
 echo "--------------------------- Question 1: At what hour of the day are tickets most likely to be issued? ---------------------------" 
 /usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q1/part-00000
 
@@ -37,7 +42,11 @@ echo "--------------------------- Question 2.2: Most common type to be ticketed 
 /usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q2/car_type/part-00000
 
 echo "--------------------------- Question 3: Most common state to be ticketed (where) ---------------------------" 
-/usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q2/part-00000
+/usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q3/part-00000
+
+echo "--------------------------- Question 3: Most likely color to be ticketed (where) ---------------------------" 
+/usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q4/part-00000
+
 
 /usr/local/hadoop/bin/hdfs dfs -rm -r /p1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /p1/output/
