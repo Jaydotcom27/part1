9f166121f353656734260ba5b778f017bab4c4bc
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-03-25 19:40:43 -0400

    added state mapper

--- part1.sh
+++ part1.sh
@@ -22,6 +22,11 @@
 -file reducer.py -reducer reducer.py \
 -input /p1/input/* -output /p1/output/Q2/car_type
 
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar  \
+-file state_mapper.py -mapper state_mapper.py \
+-file reducer.py -reducer reducer.py \
+-input /p1/input/* -output /p1/output/Q3/
+
 echo "--------------------------- Question 1: At what hour of the day are tickets most likely to be issued? ---------------------------" 
 /usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q1/part-00000
 
@@ -30,6 +35,9 @@ echo "--------------------------- Question 2.1: Most common year to be ticketed 
 
 echo "--------------------------- Question 2.2: Most common type to be ticketed ---------------------------" 
 /usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q2/car_type/part-00000
+
+echo "--------------------------- Question 3: Most common state to be ticketed (where) ---------------------------" 
+/usr/local/hadoop/bin/hdfs dfs -cat /p1/output/Q2/part-00000
 
 /usr/local/hadoop/bin/hdfs dfs -rm -r /p1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /p1/output/
